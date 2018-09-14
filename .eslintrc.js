823378f4c9994acd77006fd6aed089b5026462b1
leonardo_silva6 <leonardo.ssilva@gruporbs.com.br>
2018-09-14 14:00:58 -0300

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "extends": "airbnb",
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+        "react/jsx-filename-extension": 0,
+        "react/prefer-stateless-function": [0],
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App'
+import App from './components/App';
 
-import "./styles/init.scss";
+import './styles/init.scss';
 
 ReactDOM.render(<App />, document.getElementById('app'));
